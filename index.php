a5c3e8b68b985e22aa954720b6eae61a4a191911
Ibrahim Abdulraheem <opeibrahim@gmail.com>
2021-03-01 22:10:53 +0100

    Echo functionality and form added

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   
   <title>Registration Form</title>
+  
+  </style>
 </head>
 <body>
-
+<div class="form">
 <form method="post" action="">
 <label for="email">Email address:</label>
 <input type="email" name="email" id="" >
@@ -25,7 +26,7 @@
 </select>
         <input type="submit" name="submit">
      </form>
-
+     </div>
      <?php 
     if(isset($_POST['submit'])){ 
       $name = $_POST["name"];
